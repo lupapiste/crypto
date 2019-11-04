2f166313acbb0d282ed7d85f5e6cec4620e486ca
Joni Hämäläinen <joni.hamalainen@evolta.fi>
2019-11-04 14:07:17 +0200

    fix description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lupapiste/crypto "0.1.0"
- :description "Common stuff from Finnish SADe program"
+ :description "Crypto with bouncycastle"
  :dependencies [[org.clojure/clojure "1.10.1"]
                 [commons-codec "1.10"]
                 [crypto-random "1.2.0" :exclusions [commons-codec]]
