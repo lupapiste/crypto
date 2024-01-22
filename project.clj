6a89b1ddda00cc90df2b7d1ab855a5a806ad32bd
Lasse Määttä <lasse.maatta@cloudpermit.com>
2024-01-22 09:09:31 +0200

    Add license as required by clojars

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject lupapiste/crypto "0.1.3"
   :description "Crypto with bouncycastle"
+  :license     {:name         "EPL-2.0"
+                :url          "https://www.eclipse.org/legal/epl-2.0/"
+                :distribution :repo}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.bouncycastle/bcprov-jdk18on "1.77"]]
   :profiles {:dev {:dependencies [[midje "1.10.10" :exclusions [org.clojure/clojure]]]
